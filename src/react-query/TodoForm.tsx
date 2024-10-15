021b2b1ef81eaf03d69f8a198b0761b6a1139d0f
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-10-15 13:53:06 +0700

    Create reusable api and todo service

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -30,8 +30,9 @@ const TodoForm = () => {
           <input ref={ref} type="text" className="form-control" />
         </div>
         <div className="col">
-          <button className="btn btn-primary" disabled={addTodo.isLoading}>
-            {addTodo.isLoading ? "Adding..." : "Add"}
+          <button className="btn btn-primary">
+            {/* {addTodo.isLoading ? "Adding..." : "Add"} */}
+            Add
           </button>
         </div>
       </form>
