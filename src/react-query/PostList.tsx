a3e8d5afccfc1fed3f9373c53e9c8d5ffbb6676f
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-10-15 11:04:22 +0700

    Infinite queries

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,18 +1,15 @@
-import { useState } from "react";
 import usePosts from "./hooks/usePosts";
+import React from "react";
 
 const PostList = () => {
-  // const [userId, setUserId] = useState<number>();
-  // const { data: posts, error, isLoading } = usePosts(userId);
-
   const pageSize = 10;
-  const [page, setPage] = useState(1);
   const {
     data: posts,
     error,
     isLoading,
+    fetchNextPage,
+    isFetchingNextPage,
   } = usePosts({
-    page,
     pageSize,
   });
 
@@ -21,35 +18,23 @@ const PostList = () => {
 
   return (
     <>
-      {/* <select
-        className="form-select mb-3"
-        onChange={(event) => setUserId(+event.target.value)}
-        value={userId}
-      >
-        <option value=""></option>
-        <option value="1">User 1</option>
-        <option value="2">User 2</option>
-        <option value="3">User 3</option>
-      </select> */}
       <ul className="list-group">
-        {posts?.map((post) => (
-          <li key={post.id} className="list-group-item">
-            {post.title}
-          </li>
+        {posts.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.map((post) => (
+              <li key={post.id} className="list-group-item">
+                {post.title}
+              </li>
+            ))}
+          </React.Fragment>
         ))}
       </ul>
       <button
-        disabled={page === 1}
-        className="btn btn-primary my-3"
-        onClick={() => setPage(page - 1)}
-      >
-        Previous
-      </button>
-      <button
         className="btn btn-primary my-3 ms-1"
-        onClick={() => setPage(page + 1)}
+        disabled={isFetchingNextPage}
+        onClick={() => fetchNextPage()}
       >
-        Next
+        {isFetchingNextPage ? "Loading..." : "Load More"}
       </button>
     </>
   );
