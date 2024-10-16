3e65acd7590fa3f9ca9fdeaf7a9fe82e9afc1071
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-10-16 14:10:10 +0700

    Use Zustand to create store for counter

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import NavBar from "./state-management/NavBar";
 import HomePage from "./state-management/HomePage";
 import AuthProvider from "./state-management/auth/AuthProvider";
 import { TasksProvider } from "./state-management/tasks";
+import Counter from "./state-management/counter/Counter";
 
 function App() {
   return (
     <AuthProvider>
       <TasksProvider>
+        <Counter />
         <NavBar />
         <HomePage />
       </TasksProvider>
--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,10 +1,11 @@
 import LoginStatus from "./auth/LoginStatus";
+import useCounterStore from "./counter/store";
 
 const NavBar = () => {
-  // const { tasks } = useTasks();
+  const { counter } = useCounterStore();
   return (
     <nav className="navbar d-flex justify-content-between">
-      <span className="badge text-bg-secondary">4</span>
+      <span className="badge text-bg-secondary">{counter}</span>
       <LoginStatus />
     </nav>
   );
