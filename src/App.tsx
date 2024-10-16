ccc873027c0789073e7744ed1310f127781d0456
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-10-16 10:49:08 +0700

    Create custom provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,18 @@ import tasksReducer from "./state-management/reducers/tasksReducer";
 import NavBar from "./state-management/NavBar";
 import HomePage from "./state-management/HomePage";
 import TasksContext from "./state-management/context/tasksContext";
-import loginStatusReducer from "./state-management/reducers/loginStatusReducer";
-import AuthContext from "./state-management/context/authContext";
+import AuthProvider from "./state-management/AuthProvider";
 
 function App() {
   const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
-  const [user, authDispatch] = useReducer(loginStatusReducer, "");
 
   return (
-    <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+    <AuthProvider>
       <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
         <NavBar />
         <HomePage />
       </TasksContext.Provider>
-    </AuthContext.Provider>
+    </AuthProvider>
   );
 }
 
