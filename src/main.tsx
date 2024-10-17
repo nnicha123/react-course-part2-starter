f9df06e45004dbec5924553c9221309d6a98b26b
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-10-17 09:57:30 +0700

    Setup routing and getting information about current route

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import App from "./App";
 import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routing/routes";
 
 // new QueryClient({
 //   defaultOptions: {
@@ -24,7 +25,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router}></RouterProvider>
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,10 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  const [searchParms, setSearchParams] = useSearchParams();
+  const location = useLocation();
+  console.log(location);
   return <p>User</p>;
 };
 
